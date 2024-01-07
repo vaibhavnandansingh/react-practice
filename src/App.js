637f502a68ba6fda0231af16c467c399ee14581c
vaibhavnandansingh <vaibhavnandansingh@gmail.com>
2024-01-07 18:54:30 +0530

    react basic form with validation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,23 @@
 import { useState } from 'react';
 import './App.css';
-// import Button from 'react-bootstrap/Button';
 
-const save = (event) =>{
-  event.preventDefault();
-  var data = new FormData(event.target);
-  let formObject = Object.fromEntries(data.entries());
-  console.log(formObject);
-}
 
+const gender = ["Male", "Female", "Other"];
 const initialValues = {
-  fname:"",
-  lname:"",
-  email:"",
-  phone:"",
-  address:"",
-  dob:"",
-  state:""
+  fname: "",
+  lname: "",
+  email: "",
+  phone: "",
+  address: "",
+  dob: "",
+  state: "",
+  personGender: gender
 };
 
 function App() {
-  const[values, setValues] = useState(initialValues);
-  const[radioBtn, setRadioBtn] = useState();
+  const [values, setValues] = useState(initialValues);
+  //const [radioBtn, setRadioBtn] = useState();
+  const [errorMsg, setErrorMsg] = useState({});
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
@@ -31,9 +27,28 @@ function App() {
     });
   };
 
-  const handleRadioBtn = (e) =>{
-    setRadioBtn(e.target.value);
+  const save = (event) => {
+    event.preventDefault();
+    var data = new FormData(event.target);
+    let formObject = Object.fromEntries(data.entries());
+    console.log(formObject);
+    setErrorMsg(validate(formObject));
   }
+  
+  const validate = (values) => {
+    console.log(values);
+    const err = {};
+    if(!values.fname){
+      err.fname = "First name required."
+    }
+    if(!values.lname){
+      err.lname = "Last name required."
+    }
+    return err;
+  }
+  // const handleRadioBtn = (e) => {
+  //   setRadioBtn(e.target.value);
+  // }
 
   return (
     <form onSubmit={save}>
@@ -41,28 +56,30 @@ function App() {
         <h1>React Form Pracrice</h1>
         <div>
           <p>First Name:</p>
-          <input type='text' name='fname' placeholder='Please enter first name.' value={values.fname} 
-          onChange={handleInputChange} />
+          <input type='text' name='fname' placeholder='Please enter first name.' value={values.fname}
+            onChange={handleInputChange} />
+            <p>{errorMsg.fname}</p>
         </div>
         <div>
           <p>Last Name:</p>
-          <input type='text' name='lname' placeholder='Please enter last name.' value={values.lname} 
-          onChange={handleInputChange} />
+          <input type='text' name='lname' placeholder='Please enter last name.' value={values.lname}
+            onChange={handleInputChange} />
+            <p>{errorMsg.lname}</p>
         </div>
         <div>
           <p>Email:</p>
           <input type='email' name='email' placeholder='Please enter email id.' value={values.email}
-          onChange={handleInputChange} />
+            onChange={handleInputChange} />
         </div>
         <div>
           <p>Phone no:</p>
-          <input type='text' name='phone' placeholder='Please enter phone no.' value={values.phone} 
-          onChange={handleInputChange} />
+          <input type='text' name='phone' placeholder='Please enter phone no.' value={values.phone}
+            onChange={handleInputChange} />
         </div>
         <div>
           <p>Address:</p>
           <textarea placeholder='Please enter addrss.' name='address' value={values.address}
-          onChange={handleInputChange}></textarea>
+            onChange={handleInputChange}></textarea>
         </div>
         <div>
           <p>Date of birth:</p>
@@ -79,14 +96,14 @@ function App() {
         </div>
         <div>
           <p>Gender:</p>
-          <input type="radio" id="maleGender" name="personGender" value="male" onChange={handleRadioBtn}
-          checked={radioBtn === "male"} />
+          <input type="radio" id="maleGender" name="personGender" value={initialValues.personGender[0]} onChange={handleInputChange}
+            checked={values.personGender === initialValues.personGender[0]} />
           <label htmlFor="maleGender">Male</label>
-          <input type="radio" id="femaleGender" name="personGender" value="female" onChange={handleRadioBtn}
-          checked={radioBtn === "female"}/>
+          <input type="radio" id="femaleGender" name="personGender" value={initialValues.personGender[1]} onChange={handleInputChange}
+            checked={values.personGender === initialValues.personGender[1]} />
           <label htmlFor="femaleGender">Female</label>
-          <input type="radio" id="otherGender" name="personGender" value="other" onChange={handleRadioBtn}
-          checked={radioBtn === "other"} />
+          <input type="radio" id="otherGender" name="personGender" value={initialValues.personGender[2]} onChange={handleInputChange}
+            checked={values.personGender === "Other"} />
           <label htmlFor="otherGender">Other</label>
         </div>
         <button>Submit</button>
